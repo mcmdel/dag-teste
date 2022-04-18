b095e9e35b90a685522549d6b1b9f5d64979b4ec
mmedeiros <mmedeiros@mtrix.com.br>
2022-04-18 15:14:23 -0300

    Inclusão de DAG de simulação.

---
+++ spark_csv_raw_dag.py
@@ -0,0 +1,46 @@
+# internal library
+import time
+import random
+
+from airflow.decorators import dag, task
+
+############################################################
+# Default DAG arguments
+############################################################
+
+default_args = {
+    "owner": "mtrix",
+    "depends_on_past": False,
+    "email": "mmedeiros@mtrix.com.br", # Make sure you create the "email_monitoring" variable in the Airflow interface
+    "email_on_failure": True,
+    "email_on_retry": False
+}
+
+@dag(
+     dag_id= 'spark_csv_raw_dag',
+     description= 'Execução do Spark JOB para carga de dados na Raw',
+     catchup=False,
+     default_args= default_args,
+     tags=['simulation'],
+)
+def spark_job_csv():
+    """
+    ### Execução de spark job
+    """
+    @task()
+    def spark_csv_raw():
+        """
+        #### Extract task
+        A simple Extract task to get data ready for the rest of the data
+        pipeline. In this case, getting data is simulated by reading from a
+        hardcoded JSON string.
+        """
+        faixa = random.choice([2,5,7,20,30,60,75,80,100,240])
+        tempo = random.randint(1, faixa)
+
+        time.sleep(tempo)
+
+        return True
+    service_spark = spark_csv_raw()
+
+spark_csv_raw_dag = spark_job_csv()
