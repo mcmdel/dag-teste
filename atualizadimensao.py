e4dcb64dcf2736323a50dc9a99a7b9dba0f5b62f
cbarbosa <cbarbosa@mtric.com.br>
2022-04-24 18:27:35 -0300

    atualizacao Forn. AD

--- atualizadimensao.py
+++ atualizadimensao.py
@@ -90,7 +90,7 @@ def carga_trino():
     print ("Sucesso")
 
 
-    [atualiza_prd_Fornecedor(),atualiza_prd_AD(),atualiza_cnpj_fornecedor(), atualiza_cnpj_ad() >> envia_email()]
+    [atualiza_prd_Fornecedor(),atualiza_prd_AD(),atualiza_cnpj_fornecedor(), atualiza_cnpj_ad()] >> envia_email()
 
 carga_trino_dag = carga_trino()
 
