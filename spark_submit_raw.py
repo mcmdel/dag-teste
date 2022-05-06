884d91681e1a8c6b7000ca667d16b510616632f8
mmedeiros <mmedeiros@mtrix.com.br>
2022-05-05 21:02:37 -0300

    teste operador spark-submit (exemplo)

--- spark_submit_raw.py
+++ spark_submit_raw.py
@@ -1,20 +1,16 @@
-import airflow
-from datetime import datetime, timedelta
-from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
+from datetime import datetime
 
-default_args = {
-    'owner': 'mtrix',
-    'start_date': datetime(2022, 5, 5),
-    'retries': 3,
-	  'retry_delay': timedelta(minutes=1)
-}
-with airflow.DAG('dag_spark_submit_raw',
-                  default_args=default_args) as dag:
-    task_elt_documento_pagar = SparkSubmitOperator(
-        task_id='py_spark',
-        conn_id='spark_default',
-        application="local:/app/processar.py",
-        application_args=["teste"], #caso precise enviar dados da dag para o job airflow utilize esta propriedade
-        total_executor_cores=2,
-        executor_memory="1g"
+from airflow.models import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
+with DAG(
+    dag_id='example_spark_operator',
+    schedule_interval=None,
+    start_date=datetime(2022, 5, 5),
+    catchup=False,
+    tags=['example'],
+) as dag:
+    # [START howto_operator_spark_submit]
+    submit_job = SparkSubmitOperator(
+        application="/opt/bitnami/spark/examples/src/main/python/pi.py", task_id="submit_job"
     )
